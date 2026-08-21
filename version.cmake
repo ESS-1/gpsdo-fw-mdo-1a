b0315ce755762d138a6fb99dc31de5cdd04f7fd8
Eugene Shelkovin <eugene.shelkovin@gmail.com>
2026-08-21 06:22:25 +0300

    Reserve 16K for bootloader and generate UF2

--- version.cmake
+++ version.cmake
@@ -1,4 +1,5 @@
 # Firmware version and type
-set(FW_VERSION "0.1.23")
-set(FW_TYPE    "MDO")
-set(FW_MODEL   "MDO-1A")
+set(FW_VERSION    "0.1.23")
+set(FW_TYPE       "MDO")
+set(FW_MODEL      "MDO-1A")
+set(UF2_FAMILY_ID "0xCA8A701A")
