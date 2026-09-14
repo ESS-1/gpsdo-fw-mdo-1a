a30e40c5a9b575492e15e9df473f7594309c1658
Eugene Shelkovin <eugene.shelkovin@gmail.com>
2026-09-14 08:30:24 +0300

    Fix metadata in .ld

--- src/app_metadata.c
+++ src/app_metadata.c
@@ -2,13 +2,11 @@
 #include <stdint.h>
 
 typedef struct __attribute__((packed)) {
-    char     hardware_id[10];
-    char     app_version[10];
-    uint32_t crc32;
+    char     hardware_id[10] __attribute__((nonstring));
+    char     app_version[10] __attribute__((nonstring));
 } app_metadata_t;
 
-__attribute__((section(".metadata"), used)) const app_metadata_t g_app_metadata = {
+__attribute__((section(".app_metadata"), used)) const app_metadata_t g_app_metadata = {
     .hardware_id = BUILD_FW_HWID,
     .app_version = BUILD_FW_VERSION,
-    .crc32       = 0xCCCCCCCC, // Placeholder for CRC32, to be filled in by the build system
 };
